2408abf5a08236d71593d2c61f3912fe74e8724a
Furkan Leylek <furkanyasinn.1@outlook.com>
2023-03-26 22:58:16 +0300

    Update loginModal.jsx

--- components/modals/loginModal.jsx
+++ components/modals/loginModal.jsx
@@ -9,7 +9,7 @@ function LoginModal({ login, setLogin, }) {
     after:content[''] after:opacity-50 after:w-full after:h-full after:bg-black after:fixed after:inset-0 z-50 overflow-y-auto
     ">
             <OutsideClickHandler onOutsideClick={() => setLogin(false)} display="contents">
-                <div className='shadow-modal w-full exsm:w-[70%] sm:w-[50%] lg:w-[40%] max-w-[450px] animate-modal rounded h-[70%] bg-[#F1F1E8]  opacity-10 relative z-50'>
+                <div className='shadow-modal w-full exsm:w-[70%] sm:w-[50%] lg:w-[40%] max-w-[450px] animate-modal overflow-y-hidden rounded h-full max-h-[600px] bg-[#F1F1E8]  opacity-10 relative z-50'>
                     {login && (
                         < Login setLogin={setLogin} />
                     )}
